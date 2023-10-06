f53907eeefc9c68b055f18b03cc3eaa356ff07f8
rofikul-resan <resan6231@gmail.com>
2023-10-06 20:34:15 +0600

    add footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Footer from "./components/Footer";
 import NavigationBar from "./components/NavigationBar";
 
 export default function Home() {
   return (
     <main>
       <NavigationBar />
+      <Footer />
     </main>
   );
 }

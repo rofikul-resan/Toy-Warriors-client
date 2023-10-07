0a00dd644dad97264e36be31468c34f7cd9c22a2
rofikul-resan <resan6231@gmail.com>
2023-10-07 23:03:00 +0600

    add caregory section

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import AboutStats from "@/components/AboutStats";
 import Banner from "@/components/Banner/Banner";
 import Footer from "@/components/Footer";
 import GallerySection from "@/components/GallerySection";
+import HomeToyCategory from "@/components/HomeToyCategory";
 import NavigationBar from "@/components/NavigationBar";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <NavigationBar />
       <Banner />
       <AboutStats />
+      <HomeToyCategory />
       <GallerySection />
       <Footer />
     </main>
